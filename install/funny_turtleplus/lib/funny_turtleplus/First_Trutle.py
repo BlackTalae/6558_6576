06599c56bed107e5c974af39c474c2cf4f40c0b1
CharlotteXIII <sippanon2546@gmail.com>
2024-09-14 23:40:11 +0700

    Test_01
    about display

--- install/funny_turtleplus/lib/funny_turtleplus/First_Trutle.py
+++ install/funny_turtleplus/lib/funny_turtleplus/First_Trutle.py
@@ -2,83 +2,141 @@
 
 from funny_turtleplus.dummy_module import dummy_function, dummy_var
 import rclpy
-import math
-import numpy as np
 from rclpy.node import Node
-from geometry_msgs.msg import Twist, Point, TransformStamped, PoseStamped
-from turtlesim.msg import Pose
+from teleop_interfaces.srv import SavePath, ClearPath, MakePath
 from turtlesim_plus_interfaces.srv import GivePosition
+from turtlesim.msg import Pose
 from std_srvs.srv import Empty
-from nav_msgs.msg import Odometry
-from tf2_ros import TransformBroadcaster
-from tf_transformations import quaternion_from_euler
-from std_msgs.msg import Float64
-from teleop_interfaces.srv import ClearPath, MakePath,SavePath
-from rcl_interfaces.msg import SetParametersResult
+import numpy as np
+import math
+from geometry_msgs.msg import Twist
+import yaml
 
-class DummyNode(Node):
+class TeleopScheduleNode(Node):
     def __init__(self):
-        super().__init__('First_Turtle')
-        self.create_subscription(Pose, '/turtle1/pose', self.pose_callback, 10) 
-        self.robot_param = np.array([0.0, 0.0, 0.0]) #xytheta
-        self.count = 0
-
-        #for Spawn pizza
-        self.sp_pizza_client = self.create_client(GivePosition, '/spawn_pizza')
-        self.declare_parameter('pizza_number',3 )
-        self.pizza = self.get_parameter('pizza_number').value
-        self.add_on_set_parameters_callback(self.parameter_update_callback)
-        print('You have',self.pizza,'Pizza')
+        super().__init__('teleop_schedule_node')
 
         self.make_service = self.create_service(MakePath, '/make_path', self.make_service_callback)
         self.save_service = self.create_service(SavePath, '/save_path', self.save_service_callback)
         self.clear_service = self.create_service(ClearPath, '/clear_path', self.clear_service_callback)
+
+        self.spawn_pizza_client = self.create_client(GivePosition, '/spawn_pizza')
+        self.eat_pizza_client = self.create_client(Empty, 'eat')
+        self.done_client = self.create_client(MakePath, '/done')
+        #a
+        self.create_subscription(Pose, '/turtle1/pose', self.pose_callback, 10)
+        #a
+        self.cmd_vel_pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+
+        self.robot_pose = np.array([0.0, 0.0, 0.0]) 
+
+        self.path_x = []
+        self.path_y = []
+
+        self.trig = 1
+        self.count = 0
+
+        self.timer_turtle = self.create_timer(1.0/100.0, self.turtle_run)
  
-        #for Clear pizza
-        self.pizzaX = []
-        self.pizzaY = []
 
-    def parameter_update_callback(self, params):
-        for param in params:
-            if param.name == 'pizza_number':
-                self.pizza = param.value
-                print('Number of pizza have change to',self.pizza)
-        return SetParametersResult(successful=True)
-    
-    def pose_callback(self, msg):
-        self.robot_param[0] = msg.x
-        self.robot_param[1] = msg.y
-        self.robot_param[2] = msg.theta
+    def make_service_callback(self, request:MakePath.Request, response:MakePath.Response):
+        print("Press i button")
+        position_request = GivePosition.Request() # client -> request
 
-    def spawn_pizza(self, x, y):
-        position_req = GivePosition.Request()
-        position_req.x = x
-        position_req.y = y
-        self.sp_pizza_client.call_async(position_req)
+        position_request.x = self.robot_pose[0]
+        position_request.y = self.robot_pose[1]
 
-    def make_service_callback(self, request: MakePath.Request, response: MakePath.Response):
-        if self.count < self.pizza:
-            self.spawn_pizza(self.robot_param[0], self.robot_param[1])
-            self.pizzaX.append(self.robot_param[0])
-            self.pizzaY.append(self.robot_param[1])
-            self.count += 1
-            print(f'Pizza: {self.count} / {int(self.pizza)}')
-        else:
-            print('No more pizza!')
-        print('Pizza X positions:', self.pizzaX)
-        print('Pizza Y positions:', self.pizzaY)
+        self.path_x.append(float(position_request.x))
+        self.path_y.append(float(position_request.y))
+
+        print(f"x : {self.path_x} / y : {self.path_y}")
+
+        self.spawn_pizza_client.call_async(position_request) # call when click on GUI
         return response
 
+    def pose_callback(self, msg):
+        self.robot_pose[0] = msg.x
+        self.robot_pose[1] = msg.y
+        self.robot_pose[2] = msg.theta
+
+    def eat_pizza(self):
+        eat_request = Empty.Request()
+        self.eat_pizza_client.call_async(eat_request)
+
+    def clear_pizza(self):
+        K_d = 3.2
+        K_theta = 5.6
+        if len(self.path_x) != 0:
+            delta_x = self.path_x[0] - self.robot_pose[0]
+            delta_y = self.path_y[0] - self.robot_pose[1]
+            d = np.sqrt(delta_x**2 + delta_y**2)
+            theta = math.atan2(delta_y, delta_x)
+            e_angle = theta - self.robot_pose[2]
+            e_angle_new = math.atan2(math.sin(e_angle), math.cos(e_angle))
+            vx = K_d * d
+            wz = K_theta * e_angle_new
+            self.cmdvel(vx, wz) # use function cmdvel
+            if (d < 0.8):
+                self.eat_pizza()
+                self.path_x.pop(0)
+                self.path_y.pop(0)
+        else:
+            self.trig = 1
 
     def save_service_callback(self, request:SavePath.Request, response:SavePath.Response):
+        response.count += 1
+        self.count += 1
+
+        # Path to the YAML file
+        yaml_file_path = '/home/talae-ubantu/6558_6576/src/funny_turtleplus/config/funny_yaml.yaml'
+        
+        # Read the YAML file
+        with open(yaml_file_path, 'r') as file:
+            params = yaml.safe_load(file)
+
+        # Modify the parameter in the YAML structure
+        if 'copy_schedule_node' in params and 'ros__parameters' in params['copy_schedule_node']:
+            # self.get_logger().info(f"Current motor_target: {params['copy_schedule_node']['ros__parameters']['motor_target']}")
+            print(f"x: {self.path_x} / y: {self.path_y}")
+            # Update the motor_target parameter
+            params['copy_schedule_node']['ros__parameters'][f'turtle{self.count}_target_x'] = self.path_x # new value
+            params['copy_schedule_node']['ros__parameters'][f'turtle{self.count}_target_y'] = self.path_y # new value
+            
+            # self.get_logger().info(f"Updated motor_target to: {params['copy_schedule_node']['ros__parameters']['motor_target']}")
+        
+        # Write the updated parameters back to the YAML file
+        with open(yaml_file_path, 'w') as file:
+            yaml.safe_dump(params, file)
+        
+        self.get_logger().info("YAML file updated successfully.")
+
+        self.path_x = []
+        self.path_y = []
+
+        if self.count >= 4:
+            make_request = MakePath.Request()
+            self.done_client.call_async(make_request)
+            
         return response
 
     def clear_service_callback(self, request:ClearPath.Request, response:ClearPath.Response):
-        return response 
+        self.trig = 0
+        print("clear!!!!")
+        return response
+    
+    def turtle_run(self):
+        if self.trig == 0:
+            self.clear_pizza()
+
+    def cmdvel(self, v, w): # topic
+        msg = Twist() # create topic message
+        msg.linear.x = v 
+        msg.angular.z = w 
+        self.cmd_vel_pub.publish(msg) # publish topic
 
 def main(args=None):
     rclpy.init(args=args)
-    node = DummyNode()
+    node = TeleopScheduleNode()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
