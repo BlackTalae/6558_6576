547f4b98195869f0ccfd94c2a34641f45ffe9256
BlackTalae <anuwit5865@gmail.com>
2024-09-14 10:20:50 +0700

    clone turtlesimplus package

---
+++ build/turtlesim_plus/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/talae-ubantu/6558_6576/src/turtlesim_plus/turtlesim_plus
+# Build directory: /home/talae-ubantu/6558_6576/build/turtlesim_plus
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
